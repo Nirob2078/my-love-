3dd6bf87196a15f385c7f95ff0dc05fd4c6f3be1
jahin12345 <64268615+jahin12345@users.noreply.github.com>
2020-06-15 23:50:16 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var head=document.querySelector("#head");
+var pic=document.querySelector("#photo");
+
+function text1(){
+
+   
+    pic.src ="Ilove.jpg";
+   
+}
+var head2=document.querySelector("#head2");
+var pic2=document.querySelector("#photo2");
+
+function text2(){
+
+   pic2.src="broke.jpg";
+  
+}
